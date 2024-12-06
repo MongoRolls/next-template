3627a961836e2cc9d036db8f462ce589e76bdc89
xzc <xuzhichao1618@qq.com>
2024-12-06 11:45:14 +0800

    feat: 测试vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 import React from 'react';
 
-import { Button } from 'antd';
+import Image from 'next/image';
+// import { Button } from 'antd';
 
 const Home = () => {
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
+    <div style={{ width: '100%', height: '100%', backgroundColor: 'gray' }}>
+      <Image src="/images/union.png" width={100} height={100} alt="union" />
     </div>
+    // <div className="App">
+    //   <Button type="primary">Button</Button>
+    // </div>
   );
 };
 
